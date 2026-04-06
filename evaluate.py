ca9598eceff777488e9f119a7acc913fed4ca3cd
Zach Hersick <zachhersick31@gmail.com>
2026-04-06 14:27:43 -0700

    model evaluation

--- evaluate.py
+++ evaluate.py
@@ -20,16 +20,20 @@ for i, row in df.iterrows():
         df.at[i, 'target_short_long_mean_diff'] = row[f'{sensor}_short_long_mean_diff']
         
         if row['prediction'] == 0 and row['anomaly_type'] == 'drift':
-            continue
-            # print(sensor)
-            # print(row[f'{sensor}_5_step_diff'])
-            # print(row[f'{sensor}_10_step_diff'])
-            # print(row[f'{sensor}_short_long_mean_diff'])   
+            print(sensor)
+            print(row[f'{sensor}_5_step_diff'])
+            print(row[f'{sensor}_10_step_diff'])
+            print(row[f'{sensor}_short_long_mean_diff'])   
             
 drift_rows = df[
     (df['real_value'] == 1) &
     (df['anomaly_type'] == 'drift')
     ]
+
+osc_rows = df[
+    (df['real_value'] == 1) &
+    (df['anomaly_type'] == 'oscillation')
+]
 
 drift_temp = drift_rows[(drift_rows['target_sensor'] == 'temperature')]
 correct_drift_temp = drift_temp[(drift_temp['prediction'] == 1)]
@@ -59,7 +63,7 @@ missed_drift_temp_compare = missed_drift_temp[
     ]
 ].agg(['mean', 'median', 'std', 'min', 'max']).T
 
-correct_drift_current_compare = correct_drift_temp[
+correct_drift_current_compare = correct_drift_current[
     [
         'current_5_step_diff',
         'current_10_step_diff',
@@ -69,7 +73,7 @@ correct_drift_current_compare = correct_drift_temp[
     ]
 ].agg(['mean', 'median', 'std', 'min', 'max']).T
 
-missed_drift_current_compare = missed_drift_temp[
+missed_drift_current_compare = missed_drift_current[
     [
         'current_5_step_diff',
         'current_10_step_diff',
@@ -84,48 +88,98 @@ print(missed_drift_temp_compare)
 print(correct_drift_current_compare)
 print(missed_drift_current_compare)
 
-# print('correct_drift_temp')
-for i, row in correct_drift_temp.iterrows():
-    continue
-#     print(row['temperature_5_step_diff'])
-#     print(row['temperature_10_step_diff'])
-#     print(row['temperature_short_long_mean_diff'])
-#     print(row['temperature_roll_std'])
-#     print(row['temperature_roll_mean'])
-# print('------------------------')
+osc_volt = osc_rows[(osc_rows['target_sensor'] == 'voltage')]
+correct_osc_volt = osc_volt[(osc_volt['prediction'] == 1 )]
+missed_osc_volt = osc_volt[(osc_volt['prediction'] == 0)]
 
-# print('missed_drift_temp')
+osc_current = osc_rows[(osc_rows['target_sensor'] == 'current')]
+correct_osc_current = osc_current[(osc_current['prediction'] == 1 )]
+missed_osc_current = osc_current[(osc_current['prediction'] == 0)]
+
+correct_osc_volt_compare = correct_osc_volt[
+    [
+        'voltage_5_step_diff',
+        'voltage_10_step_diff',
+        'voltage_short_long_mean_diff',
+        'voltage_roll_std',
+        'voltage_roll_mean'
+    ]
+].agg(['mean', 'median', 'std', 'min', 'max']).T
+
+missed_osc_volt_compare = missed_osc_volt[
+    [
+        'voltage_5_step_diff',
+        'voltage_10_step_diff',
+        'voltage_short_long_mean_diff',
+        'voltage_roll_std',
+        'voltage_roll_mean'
+    ]
+].agg(['mean', 'median', 'std', 'min', 'max']).T
+
+correct_osc_current_compare = correct_osc_current[
+    [
+        'current_5_step_diff',
+        'current_10_step_diff',
+        'current_short_long_mean_diff',
+        'current_roll_std',
+        'current_roll_mean'
+    ]
+].agg(['mean', 'median', 'std', 'min', 'max']).T
+
+missed_osc_current_compare = missed_osc_current[
+    [
+        'current_5_step_diff',
+        'current_10_step_diff',
+        'current_short_long_mean_diff',
+        'current_roll_std',
+        'current_roll_mean'
+    ]
+].agg(['mean', 'median', 'std', 'min', 'max']).T
+
+print(correct_osc_volt_compare)
+print(missed_osc_volt_compare)
+print(correct_osc_current_compare)
+print(missed_osc_current_compare)
+
+print('correct_drift_temp')
+for i, row in correct_drift_temp.iterrows():
+    print(row['temperature_5_step_diff'])
+    print(row['temperature_10_step_diff'])
+    print(row['temperature_short_long_mean_diff'])
+    print(row['temperature_roll_std'])
+    print(row['temperature_roll_mean'])
+print('------------------------')
+
+print('missed_drift_temp')
 for i, row in missed_drift_temp.iterrows():
-    continue
-#     print(row['temperature_5_step_diff'])
-#     print(row['temperature_10_step_diff'])
-#     print(row['temperature_short_long_mean_diff'])
-#     print(row['temperature_roll_std'])
-#     print(row['temperature_roll_mean'])
-# print('-------------------------') 
+    print(row['temperature_5_step_diff'])
+    print(row['temperature_10_step_diff'])
+    print(row['temperature_short_long_mean_diff'])
+    print(row['temperature_roll_std'])
+    print(row['temperature_roll_mean'])
+print('-------------------------') 
 
 drift_voltage = drift_rows[(drift_rows['target_sensor'] == 'voltage')]
 correct_drift_voltage = drift_voltage[(drift_voltage['prediction'] == 1)]
 missed_drift_voltage = drift_voltage[(drift_voltage['prediction'] == 0)]
-# print('correct_drift_voltage')
-for i, row in correct_drift_voltage.iterrows():
-    continue
-#     print(row['voltage_5_step_diff'])
-#     print(row['voltage_10_step_diff'])
-#     print(row['voltage_short_long_mean_diff'])
-#     print(row['voltage_roll_std'])
-#     print(row['voltage_roll_mean'])
-# print('-------------------------') 
 
-# print('missed_drift_voltage')
+print('correct_drift_voltage')
+for i, row in correct_drift_voltage.iterrows():
+    print(row['voltage_5_step_diff'])
+    print(row['voltage_10_step_diff'])
+    print(row['voltage_short_long_mean_diff'])
+    print(row['voltage_roll_std'])
+    print(row['voltage_roll_mean'])
+print('-------------------------') 
+
+print('missed_drift_voltage')
 for i, row in missed_drift_voltage.iterrows():
-    continue
-    # print(row['voltage_5_step_diff'])
-    # print(row['voltage_10_step_diff'])
-    # print(row['voltage_short_long_mean_diff'])
-    # print(row['voltage_roll_std'])
-    # print(row['voltage_roll_mean'])
-# print('-------------------------') 
+    print(row['voltage_5_step_diff'])
+    print(row['voltage_10_step_diff'])
+    print(row['voltage_short_long_mean_diff'])
+    print(row['voltage_roll_std'])
+    print(row['voltage_roll_mean'])
+print('-------------------------') 
 
 drift_by_sensor = drift_rows.groupby(by='target_sensor').agg(
     total=('prediction', 'size'),
@@ -135,12 +189,7 @@ drift_by_sensor = drift_rows.groupby(by='target_sensor').agg(
 
 drift_by_sensor['recall'] = drift_by_sensor['correct'] / drift_by_sensor['total']
 
-# print(drift_by_sensor)
-
-osc_rows = df[
-    (df['real_value'] == 1) &
-    (df['anomaly_type'] == 'oscillation')
-    ]
+print(drift_by_sensor)
 
 osc_by_sensor = osc_rows.groupby(by='target_sensor').agg(
     total=('prediction', 'size'),
@@ -150,7 +199,7 @@ osc_by_sensor = osc_rows.groupby(by='target_sensor').agg(
 
 osc_by_sensor['recall'] = osc_by_sensor['correct'] / osc_by_sensor['total']
 
-# print(osc_by_sensor)
+print(osc_by_sensor)
     
 correct_drift_rows = df[
     (df['anomaly_type'] == 'drift') & 
@@ -164,61 +213,61 @@ missed_drift_rows = df[
     (df['prediction'] == 0)
     ]
 
-# print('Correct drift rows:')
-# print(correct_drift_rows[
-#     ['target_5_step_diff', 'target_10_step_diff', 'target_short_long_mean_diff']
-# ].describe())
+print('Correct drift rows:')
+print(correct_drift_rows[
+    ['target_5_step_diff', 'target_10_step_diff', 'target_short_long_mean_diff']
+].describe())
 
-# print('\nMissed drift rows:')
-# print(missed_drift_rows[
-#     ['target_5_step_diff', 'target_10_step_diff', 'target_short_long_mean_diff']
-# ].describe())
+print('\nMissed drift rows:')
+print(missed_drift_rows[
+    ['target_5_step_diff', 'target_10_step_diff', 'target_short_long_mean_diff']
+].describe())
         
-# num_spike = 0
-# num_spike_correct = 0
-# num_drop = 0
-# num_drop_correct = 0
-# num_drift = 0
-# num_drift_correct = 0
-# num_oscillation = 0
-# num_oscillation_correct = 0
-# num_stuck_sensor = 0
-# num_stuck_sensor_correct = 0
-# num_impossible = 0
-# num_impossible_correct = 0
+num_spike = 0
+num_spike_correct = 0
+num_drop = 0
+num_drop_correct = 0
+num_drift = 0
+num_drift_correct = 0
+num_oscillation = 0
+num_oscillation_correct = 0
+num_stuck_sensor = 0
+num_stuck_sensor_correct = 0
+num_impossible = 0
+num_impossible_correct = 0
 
-# for i, row in df.iterrows():
-#     if row['real_value'] == 1:
-#         if row['anomaly_type'] == 'spike':
-#             num_spike += 1
-#             if row['prediction'] == 1:
-#                 num_spike_correct += 1
-#         elif row['anomaly_type'] == 'drop':
-#             num_drop += 1
-#             if row['prediction'] == 1:
-#                 num_drop_correct += 1
-#         elif row['anomaly_type'] == 'oscillation':
-#             num_oscillation += 1
-#             if row['prediction'] == 1:
-#                 num_oscillation_correct += 1
-#         elif row['anomaly_type'] == 'drift':
-#             num_drift += 1
-#             if row['prediction'] == 1:
-#                 num_drift_correct += 1
-#         elif row['anomaly_type'] == 'stuck_sensor':
-#             num_stuck_sensor += 1
-#             if row['prediction'] == 1:
-#                 num_stuck_sensor_correct += 1
-#         elif row['anomaly_type'] == 'impossible_value':
-#             num_impossible += 1
-#             if row['prediction'] == 1:
-#                 num_impossible_correct += 1
+for i, row in df.iterrows():
+    if row['real_value'] == 1:
+        if row['anomaly_type'] == 'spike':
+            num_spike += 1
+            if row['prediction'] == 1:
+                num_spike_correct += 1
+        elif row['anomaly_type'] == 'drop':
+            num_drop += 1
+            if row['prediction'] == 1:
+                num_drop_correct += 1
+        elif row['anomaly_type'] == 'oscillation':
+            num_oscillation += 1
+            if row['prediction'] == 1:
+                num_oscillation_correct += 1
+        elif row['anomaly_type'] == 'drift':
+            num_drift += 1
+            if row['prediction'] == 1:
+                num_drift_correct += 1
+        elif row['anomaly_type'] == 'stuck_sensor':
+            num_stuck_sensor += 1
+            if row['prediction'] == 1:
+                num_stuck_sensor_correct += 1
+        elif row['anomaly_type'] == 'impossible_value':
+            num_impossible += 1
+            if row['prediction'] == 1:
+                num_impossible_correct += 1
 
-# print(metrics.accuracy_score(y_test, predictions))
-# print(metrics.confusion_matrix(y_test, predictions))
-# print("spike: ", num_spike, num_spike_correct)
-# print("drop: ", num_drop, num_drop_correct)
-# print("drift: ", num_drift, num_drift_correct)
-# print("oscillation: ", num_oscillation, num_oscillation_correct)
-# print("stuck_sensor: ", num_stuck_sensor, num_stuck_sensor_correct)
-# print("impossible_value: ", num_impossible, num_impossible_correct)
+print(metrics.accuracy_score(y_test, predictions))
+print(metrics.confusion_matrix(y_test, predictions))
+print("spike: ", num_spike, num_spike_correct)
+print("drop: ", num_drop, num_drop_correct)
+print("drift: ", num_drift, num_drift_correct)
+print("oscillation: ", num_oscillation, num_oscillation_correct)
+print("stuck_sensor: ", num_stuck_sensor, num_stuck_sensor_correct)
+print("impossible_value: ", num_impossible, num_impossible_correct)
--- features.py
+++ features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 sensors = ['temperature', 'pressure', 'vibration', 'flow_rate', 'voltage', 'current']
 
@@ -11,21 +12,43 @@ long_window = 50
 
 for sensor in sensors:
     df[f'{sensor}_delta'] = grouped[sensor].diff()
-    df[f'{sensor}_abs_delta'] = grouped[sensor].diff().abs()
+    df[f'{sensor}_abs_delta'] = df[f'{sensor}_delta'].abs()
+
+    delta_sign = np.sign(df[f'{sensor}_delta'])
+    prev_sign = delta_sign.groupby(df['machine_id']).shift(1)
+    sign_flip = (
+        delta_sign * prev_sign < 0
+        ).astype(int)
+    df[f'{sensor}_sign_change_count'] = (
+        sign_flip.groupby(df['machine_id'])
+        .rolling(window)
+        .sum()
+        .reset_index(level=0, drop=True)
+    )
+    
     df[f'{sensor}_roll_mean'] = grouped[sensor].rolling(window).mean().reset_index(level=0, drop=True)
     df[f'{sensor}_long_roll_mean'] = grouped[sensor].rolling(long_window).mean().reset_index(level=0, drop=True)
     df[f'{sensor}_roll_std'] = grouped[sensor].rolling(window).std().reset_index(level=0, drop=True)
+    df[f'{sensor}_long_roll_std'] = grouped[sensor].rolling(long_window).std().reset_index(level=0, drop=True)
     df[f'{sensor}_roll_min'] = grouped[sensor].rolling(window).min().reset_index(level=0, drop=True)
     df[f'{sensor}_roll_max'] = grouped[sensor].rolling(window).max().reset_index(level=0, drop=True)
     df[f'{sensor}_dev'] = df[sensor] - df[f'{sensor}_roll_mean']
-    df[f'{sensor}_zscore'] = (
-        (df[sensor] - df[f'{sensor}_roll_mean']) /
-        df[f'{sensor}_roll_std']
-    )
+    
+    denom = df[f'{sensor}_roll_std'].where(df[f'{sensor}_roll_std'] > 1e-6, float('nan'))
+    df[f'{sensor}_zscore'] = ((df[sensor] - df[f'{sensor}_roll_mean']) / denom)
     df[f'{sensor}_5_step_diff'] = grouped[sensor].diff(5)
     df[f'{sensor}_10_step_diff'] = grouped[sensor].diff(10)
-    df[f'{sensor}_roll_avg_delta'] = grouped[f'{sensor}_delta'].rolling(window).mean().reset_index(level=0, drop=True)
+    df[f'{sensor}_roll_avg_delta'] = df.groupby('machine_id')[f'{sensor}_delta'].rolling(window).mean().reset_index(level=0, drop=True)
     df[f'{sensor}_short_long_mean_diff'] = df[f'{sensor}_roll_mean'] - df[f'{sensor}_long_roll_mean']
+    df[f'{sensor}_abs_5_step_diff'] = grouped[sensor].diff(5).abs()
+    df[f'{sensor}_abs_10_step_diff'] = grouped[sensor].diff(10).abs()
+    df[f'{sensor}_roll_range'] = df[f'{sensor}_roll_max'] - df[f'{sensor}_roll_min']
+    
+    df[f'{sensor}_roll_mean_abs_delta'] = df.groupby('machine_id')[f'{sensor}_abs_delta'].rolling(window).mean().reset_index(level=0, drop=True)
+    
+    denom = df[f'{sensor}_long_roll_std'].where(df[f'{sensor}_long_roll_std'] > 1e-6, float('nan'))
+    
+    df[f'{sensor}_variability'] = df[f'{sensor}_roll_std'] / denom
 
 df = df.dropna()
 df.to_csv('sensor_data_features.csv', index=False)
